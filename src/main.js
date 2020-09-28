1be6cf0ee9c39e2a1dcb76639b49e279a619d01c
Milesq <wmilosz88@gmail.com>
2020-09-28 10:52:46 +0200

    initial commit

---
+++ src/main.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const Discord = require('discord.js')
+const bot = new Discord.Client()
+const TOKEN = process.env.TOKEN
+
+bot.login(TOKEN)
+
+bot.on('ready', () => {
+  console.info(`Logged in as ${bot.user.tag}!`)
+})
+
+bot.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.channel.send('pong')
+  }
+})
